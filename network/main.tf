9dec2cbfb2db8e9c930aa3510085e27a381858fb
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2025-12-16 08:04:12 +0530

    terraform roboshop project setup modules

--- network/main.tf
+++ network/main.tf
@@ -6,3 +6,15 @@ resource "aws_security_group" "main" {
         Name      =  "roboshop-allow-all"
     }
 }
+
+resource "aws_vpc_security_group_ingress_rule" "allow_all_traffic_ipv4" {
+    security_group_id = aws_security_group.main.id
+    cidr_ipv4         = "0.0.0.0/0"
+    ip_protocol       = "-1"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+    security_group_id = aws_security_group.main.id
+    cidr_ipv4         = "0.0.0.0/0"
+    ip_protocol       = "-1"
+}
