215b0b039a463b81e1bba42024e655748e987c02
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2025-12-16 11:22:07 +0530

    terraform roboshop project setup modules

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 module "network" {
     source = "./network"
 }
+
+module "ec2" {
+    source                  = "./ec2"
+    ami                     = var.ami
+    instance_type           = var.instance_type
+    vpc_security_group_ids  = module.network.sg
+}
